faaee995cc4b338668105eff120c2ea8e2b9ecbf
chenyungui <linxitj2003@icloud.com>
2025-11-16 11:59:19 +0800

    add ColorPrimitive

--- Sources/StyleKit/extensions/UIColor+.swift
+++ Sources/StyleKit/extensions/UIColor+.swift
@@ -22,4 +22,17 @@ extension AppColor {
 
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
+    
+    convenience init(rgba hex: String) {
+        var hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hex = hex.replacingOccurrences(of: "#", with: "")
+        guard hex.count == 8 else { fatalError("wrong hex format") }
+        var rgba: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&rgba)
+        let r = CGFloat((rgba >> 24) & 0xFF) / 255
+        let g = CGFloat((rgba >> 16) & 0xFF) / 255
+        let b = CGFloat((rgba >>  8) & 0xFF) / 255
+        let a = CGFloat(rgba & 0xFF) / 255
+        self.init(red: r, green: g, blue: b, alpha: a)
+    }
 }
