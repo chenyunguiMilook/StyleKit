25b7cb45246035c9db87c3b0e7f70e05ab981a9c
chenyungui <linxitj2003@icloud.com>
2025-11-19 09:25:08 +0800

    generate colors

---
+++ Sources/StyleKit/Colors.swift
@@ -0,0 +1,218 @@
+//
+//  Color.swift
+//  StyleKit
+//
+//  Created by chenyungui on 2025/11/19.
+//
+
+// MARK: - Generated Color Accessors
+public enum Colors {
+    public enum Background {
+        public enum Default {
+            public static let `default` = AppColor(resource: .colorBackgroundDefaultDefault)
+            public static let hover = AppColor(resource: .colorBackgroundDefaultHover)
+            public static let secondary = AppColor(resource: .colorBackgroundDefaultSecondary)
+            public static let secondaryHover = AppColor(resource: .colorBackgroundDefaultSecondaryHover)
+            public static let tertiary = AppColor(resource: .colorBackgroundDefaultTertiary)
+            public static let tertiaryHover = AppColor(resource: .colorBackgroundDefaultTertiaryHover)
+        }
+        public enum Neutral {
+            public static let `default` = AppColor(resource: .colorBackgroundNeutralDefault)
+            public static let hover = AppColor(resource: .colorBackgroundNeutralHover)
+            public static let secondary = AppColor(resource: .colorBackgroundNeutralSecondary)
+            public static let secondaryHover = AppColor(resource: .colorBackgroundNeutralSecondaryHover)
+            public static let tertiary = AppColor(resource: .colorBackgroundNeutralTertiary)
+            public static let tertiaryHover = AppColor(resource: .colorBackgroundNeutralTertiaryHover)
+        }
+        public enum Brand {
+            public static let `default` = AppColor(resource: .colorBackgroundBrandDefault)
+            public static let hover = AppColor(resource: .colorBackgroundBrandHover)
+            public static let secondary = AppColor(resource: .colorBackgroundBrandSecondary)
+            public static let secondaryHover = AppColor(resource: .colorBackgroundBrandSecondaryHover)
+            public static let tertiary = AppColor(resource: .colorBackgroundBrandTertiary)
+            public static let tertiaryHover = AppColor(resource: .colorBackgroundBrandTertiaryHover)
+        }
+        public enum Positive {
+            public static let `default` = AppColor(resource: .colorBackgroundPositiveDefault)
+            public static let hover = AppColor(resource: .colorBackgroundPositiveHover)
+            public static let secondary = AppColor(resource: .colorBackgroundPositiveSecondary)
+            public static let secondaryHover = AppColor(resource: .colorBackgroundPositiveSecondaryHover)
+            public static let tertiary = AppColor(resource: .colorBackgroundPositiveTertiary)
+            public static let tertiaryHover = AppColor(resource: .colorBackgroundPositiveTertiaryHover)
+        }
+        public enum Warning {
+            public static let `default` = AppColor(resource: .colorBackgroundWarningDefault)
+            public static let hover = AppColor(resource: .colorBackgroundWarningHover)
+            public static let secondary = AppColor(resource: .colorBackgroundWarningSecondary)
+            public static let secondaryHover = AppColor(resource: .colorBackgroundWarningSecondaryHover)
+            public static let tertiary = AppColor(resource: .colorBackgroundWarningTertiary)
+            public static let tertiaryHover = AppColor(resource: .colorBackgroundWarningTertiaryHover)
+        }
+        public enum Danger {
+            public static let `default` = AppColor(resource: .colorBackgroundDangerDefault)
+            public static let hover = AppColor(resource: .colorBackgroundDangerHover)
+            public static let secondary = AppColor(resource: .colorBackgroundDangerSecondary)
+            public static let secondaryHover = AppColor(resource: .colorBackgroundDangerSecondaryHover)
+            public static let tertiary = AppColor(resource: .colorBackgroundDangerTertiary)
+            public static let tertiaryHover = AppColor(resource: .colorBackgroundDangerTertiaryHover)
+        }
+        public enum Disabled {
+            public static let `default` = AppColor(resource: .colorBackgroundDisabledDefault)
+        }
+        public enum Utilities {
+            public static let scrim = AppColor(resource: .colorBackgroundUtilitiesScrim)
+            public static let blanket = AppColor(resource: .colorBackgroundUtilitiesBlanket)
+            public static let overlay = AppColor(resource: .colorBackgroundUtilitiesOverlay)
+            public static let measurement = AppColor(resource: .colorBackgroundUtilitiesMeasurement)
+        }
+    }
+    public enum Text {
+        public enum Default {
+            public static let `default` = AppColor(resource: .colorTextDefaultDefault)
+            public static let secondary = AppColor(resource: .colorTextDefaultSecondary)
+            public static let tertiary = AppColor(resource: .colorTextDefaultTertiary)
+        }
+        public enum Neutral {
+            public static let `default` = AppColor(resource: .colorTextNeutralDefault)
+            public static let secondary = AppColor(resource: .colorTextNeutralSecondary)
+            public static let tertiary = AppColor(resource: .colorTextNeutralTertiary)
+            public static let onNeutral = AppColor(resource: .colorTextNeutralOnNeutral)
+            public static let onNeutralSecondary = AppColor(resource: .colorTextNeutralOnNeutralSecondary)
+            public static let onNeutralTertiary = AppColor(resource: .colorTextNeutralOnNeutralTertiary)
+        }
+        public enum Brand {
+            public static let `default` = AppColor(resource: .colorTextBrandDefault)
+            public static let secondary = AppColor(resource: .colorTextBrandSecondary)
+            public static let tertiary = AppColor(resource: .colorTextBrandTertiary)
+            public static let onBrand = AppColor(resource: .colorTextBrandOnBrand)
+            public static let onBrandSecondary = AppColor(resource: .colorTextBrandOnBrandSecondary)
+            public static let onBrandTertiary = AppColor(resource: .colorTextBrandOnBrandTertiary)
+        }
+        public enum Positive {
+            public static let `default` = AppColor(resource: .colorTextPositiveDefault)
+            public static let secondary = AppColor(resource: .colorTextPositiveSecondary)
+            public static let tertiary = AppColor(resource: .colorTextPositiveTertiary)
+            public static let onPositive = AppColor(resource: .colorTextPositiveOnPositive)
+            public static let onPositiveSecondary = AppColor(resource: .colorTextPositiveOnPositiveSecondary)
+            public static let onPositiveTertiary = AppColor(resource: .colorTextPositiveOnPositiveTertiary)
+        }
+        public enum Warning {
+            public static let `default` = AppColor(resource: .colorTextWarningDefault)
+            public static let secondary = AppColor(resource: .colorTextWarningSecondary)
+            public static let tertiary = AppColor(resource: .colorTextWarningTertiary)
+            public static let onWarning = AppColor(resource: .colorTextWarningOnWarning)
+            public static let onWarningSecondary = AppColor(resource: .colorTextWarningOnWarningSecondary)
+            public static let onWarningTertiary = AppColor(resource: .colorTextWarningOnWarningTertiary)
+        }
+        public enum Danger {
+            public static let `default` = AppColor(resource: .colorTextDangerDefault)
+            public static let secondary = AppColor(resource: .colorTextDangerSecondary)
+            public static let tertiary = AppColor(resource: .colorTextDangerTertiary)
+            public static let onDanger = AppColor(resource: .colorTextDangerOnDanger)
+            public static let onDangerSecondary = AppColor(resource: .colorTextDangerOnDangerSecondary)
+            public static let onDangerTertiary = AppColor(resource: .colorTextDangerOnDangerTertiary)
+        }
+        public enum Disabled {
+            public static let `default` = AppColor(resource: .colorTextDisabledDefault)
+            public static let onDisabled = AppColor(resource: .colorTextDisabledOnDisabled)
+        }
+        public enum Utilities {
+            public static let textOnOverlay = AppColor(resource: .colorTextUtilitiesTextOnOverlay)
+            public static let textOnMeasurement = AppColor(resource: .colorTextUtilitiesTextOnMeasurement)
+        }
+    }
+    public enum Border {
+        public enum Default {
+            public static let `default` = AppColor(resource: .colorBorderDefaultDefault)
+            public static let secondary = AppColor(resource: .colorBorderDefaultSecondary)
+            public static let tertiary = AppColor(resource: .colorBorderDefaultTertiary)
+        }
+        public enum Neutral {
+            public static let `default` = AppColor(resource: .colorBorderNeutralDefault)
+            public static let secondary = AppColor(resource: .colorBorderNeutralSecondary)
+            public static let tertiary = AppColor(resource: .colorBorderNeutralTertiary)
+        }
+        public enum Brand {
+            public static let `default` = AppColor(resource: .colorBorderBrandDefault)
+            public static let secondary = AppColor(resource: .colorBorderBrandSecondary)
+            public static let tertiary = AppColor(resource: .colorBorderBrandTertiary)
+        }
+        public enum Positive {
+            public static let `default` = AppColor(resource: .colorBorderPositiveDefault)
+            public static let secondary = AppColor(resource: .colorBorderPositiveSecondary)
+            public static let tertiary = AppColor(resource: .colorBorderPositiveTertiary)
+        }
+        public enum Warning {
+            public static let `default` = AppColor(resource: .colorBorderWarningDefault)
+            public static let secondary = AppColor(resource: .colorBorderWarningSecondary)
+            public static let tertiary = AppColor(resource: .colorBorderWarningTertiary)
+        }
+        public enum Danger {
+            public static let `default` = AppColor(resource: .colorBorderDangerDefault)
+            public static let secondary = AppColor(resource: .colorBorderDangerSecondary)
+            public static let tertiary = AppColor(resource: .colorBorderDangerTertiary)
+        }
+        public enum Disabled {
+            public static let `default` = AppColor(resource: .colorBorderDisabledDefault)
+        }
+        public enum Utilities {
+            public static let measurement = AppColor(resource: .colorBorderUtilitiesMeasurement)
+            public static let swatch = AppColor(resource: .colorBorderUtilitiesSwatch)
+        }
+    }
+    public enum Icon {
+        public enum Default {
+            public static let `default` = AppColor(resource: .colorIconDefaultDefault)
+            public static let secondary = AppColor(resource: .colorIconDefaultSecondary)
+            public static let tertiary = AppColor(resource: .colorIconDefaultTertiary)
+        }
+        public enum Neutral {
+            public static let `default` = AppColor(resource: .colorIconNeutralDefault)
+            public static let secondary = AppColor(resource: .colorIconNeutralSecondary)
+            public static let tertiary = AppColor(resource: .colorIconNeutralTertiary)
+            public static let onNeutral = AppColor(resource: .colorIconNeutralOnNeutral)
+            public static let onNeutralSecondary = AppColor(resource: .colorIconNeutralOnNeutralSecondary)
+            public static let onNeutralTertiary = AppColor(resource: .colorIconNeutralOnNeutralTertiary)
+        }
+        public enum Brand {
+            public static let `default` = AppColor(resource: .colorIconBrandDefault)
+            public static let secondary = AppColor(resource: .colorIconBrandSecondary)
+            public static let tertiary = AppColor(resource: .colorIconBrandTertiary)
+            public static let onBrand = AppColor(resource: .colorIconBrandOnBrand)
+            public static let onBrandSecondary = AppColor(resource: .colorIconBrandOnBrandSecondary)
+            public static let onBrandTertiary = AppColor(resource: .colorIconBrandOnBrandTertiary)
+        }
+        public enum Positive {
+            public static let `default` = AppColor(resource: .colorIconPositiveDefault)
+            public static let secondary = AppColor(resource: .colorIconPositiveSecondary)
+            public static let tertiary = AppColor(resource: .colorIconPositiveTertiary)
+            public static let onPositive = AppColor(resource: .colorIconPositiveOnPositive)
+            public static let onPositiveSecondary = AppColor(resource: .colorIconPositiveOnPositiveSecondary)
+            public static let onPositiveTertiary = AppColor(resource: .colorIconPositiveOnPositiveTertiary)
+        }
+        public enum Warning {
+            public static let `default` = AppColor(resource: .colorIconWarningDefault)
+            public static let secondary = AppColor(resource: .colorIconWarningSecondary)
+            public static let tertiary = AppColor(resource: .colorIconWarningTertiary)
+            public static let onWarning = AppColor(resource: .colorIconWarningOnWarning)
+            public static let onWarningSecondary = AppColor(resource: .colorIconWarningOnWarningSecondary)
+            public static let onWarningTertiary = AppColor(resource: .colorIconWarningOnWarningTertiary)
+        }
+        public enum Danger {
+            public static let `default` = AppColor(resource: .colorIconDangerDefault)
+            public static let secondary = AppColor(resource: .colorIconDangerSecondary)
+            public static let tertiary = AppColor(resource: .colorIconDangerTertiary)
+            public static let onDanger = AppColor(resource: .colorIconDangerOnDanger)
+            public static let onDangerSecondary = AppColor(resource: .colorIconDangerOnDangerSecondary)
+            public static let onDangerTertiary = AppColor(resource: .colorIconDangerOnDangerTertiary)
+        }
+        public enum Disabled {
+            public static let `default` = AppColor(resource: .colorIconDisabledDefault)
+            public static let onDisabled = AppColor(resource: .colorIconDisabledOnDisabled)
+        }
+        public enum Utilities {
+            public static let icon = AppColor(resource: .colorIconUtilitiesIcon)
+            public static let iconOnMeasurement = AppColor(resource: .colorIconUtilitiesIconOnMeasurement)
+        }
+    }
+}
